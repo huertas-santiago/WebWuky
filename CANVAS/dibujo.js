15a46c7366d01d82f6d2fb0820b4e909219661aa
Santiago Huertas <huertas_santiago@outlook.es>
2021-09-09 21:56:30 -0500

    Update dibujo.js

--- CANVAS/dibujo.js
+++ CANVAS/dibujo.js
@@ -27,6 +27,7 @@ function dibujoClick()
     var yi, xf;
     var colorcito = "red";
     var espacio = ancho / lineas; 
+    console.log("Cabio de santiago")
     
     for(l; l < lineas; l++)
     {
