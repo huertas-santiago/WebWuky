4f301506b24c16fb9d877cb7bc9ae4e44593274d
PaulaStefania <89435401+PaulaStefania@users.noreply.github.com>
2022-06-08 21:22:58 -0500

    Consulta de tablas individuales

---
+++ MODELOS/BASE DE DATOS/Querys/DML/Consultas.sql
@@ -0,0 +1,45 @@
+
+use dbwuky;
+
+SELECT*FROM Marca;
+SELECT*FROM tipo_contrato;
+SELECT*FROM entidad_afiliada;
+SELECT*FROM rol;
+SELECT*FROM permiso;
+SELECT*FROM rol_permiso;
+SELECT*FROM usuario;
+SELECT*FROM cliente;
+SELECT*FROM empleado;
+SELECT*FROM afiliacion;
+SELECT*FROM proveedor;
+SELECT*FROM transportadora;
+SELECT*FROM producto;
+SELECT*FROM variante;
+SELECT*FROM material;
+SELECT*FROM variante_material;
+SELECT*FROM categoria;
+SELECT*FROM categoria_producto;
+SELECT*FROM imagen;
+SELECT*FROM infografia;
+SELECT*FROM imagenes_infografia;
+SELECT*FROM telefono;
+SELECT*FROM producto_favorito;
+SELECT*FROM pedido;
+SELECT*FROM estados_pedidos;
+SELECT*FROM envio;
+SELECT*FROM estados_facturas_cliente;
+SELECT*FROM factura_proveedor;
+SELECT*FROM carrito;
+SELECT*FROM tema_preguntas;
+SELECT*FROM preguntas_frecuentes;
+SELECT*FROM logs_empleado;
+SELECT*FROM logs_cliente;
+SELECT*FROM acciones_realizadas;
+SELECT*FROM información;
+SELECT*FROM producto_imagenes;
+SELECT*FROM producto_pedido;
+SELECT*FROM producto_facturaProveedor;
+SELECT*FROM especie;
+SELECT*FROM raza;
+SELECT*FROM mascota;
+
