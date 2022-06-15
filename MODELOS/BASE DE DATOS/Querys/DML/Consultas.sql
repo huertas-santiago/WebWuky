a677051894b2654dd9ec60c8c3f640689b9541a8
PaulaStefania <isalyvega2017@gmail.com>
2022-06-14 21:33:39 -0500

    Update Consultas.sql

--- MODELOS/BASE DE DATOS/Querys/DML/Consultas.sql
+++ MODELOS/BASE DE DATOS/Querys/DML/Consultas.sql
@@ -48,9 +48,11 @@ SELECT*FROM mascota;
 SELECT usuario.nombre, cliente.id_usuario
 from usuario inner join cliente on usuario.id_usuario=cliente.id_usuario;
 
+#Buscar mascotas de los clientes
 SELECT cliente.id_usuario, usuario.nombre, mascota.nombre
 from cliente inner join usuario on cliente.id_usuario=usuario.id_usuario
-inner join mascota on cliente.id_usuario = mascota.id_cliente;
+inner join mascota on cliente.id_usuario = mascota.id_cliente ;
 
 SELECT count(genero) FROM usuario GROUP BY genero having genero= 'M';
 SELECT count(genero) FROM usuario GROUP BY genero having genero= 'F';
+
